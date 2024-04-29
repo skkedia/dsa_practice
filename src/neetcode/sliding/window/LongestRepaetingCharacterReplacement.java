1a1807657992b410759b835f93c134fd12718f10
siddhant.kedia <siddhant.kedia@highradius.com>
2024-04-29 10:24:09 +0530

    corrected documentation

--- src/neetcode/sliding/window/LongestRepaetingCharacterReplacement.java
+++ src/neetcode/sliding/window/LongestRepaetingCharacterReplacement.java
@@ -11,8 +11,9 @@ public class LongestRepaetingCharacterReplacement {
 		while (r < s.length()) {
 			ch[s.charAt(r) - 'A']++;
 			maxo = Math.max(maxo, ch[s.charAt(r) - 'A']);
-			// we get the current window size by r - l, then we subtract maxo to get the
-			// number of diff characters in that window and we add 1 for the current char as
+			// we get the current window size by r - l, then we subtract 'maxo' to get the
+			// number of different characters in that window and we add 1 for the current
+			// char as
 			// r is updated later in the flow,
 			// if this greater than k then we have exhausted all the operations that we can
 			// perform
