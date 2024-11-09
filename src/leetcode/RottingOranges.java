6b6c168af9d69a4e9a1f29e6a8f937f0e21d94d0
siddhant.kedia <siddhant.kedia@highradius.com>
2024-11-09 12:13:25 +0530

    changes

--- src/leetcode/RottingOranges.java
+++ src/leetcode/RottingOranges.java
@@ -1,54 +1,51 @@
-package graph;
+package leetcode;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class RottenOranges {
-	
+public class RottingOranges {
+
 	public int orangesRotting(int[][] grid) {
-		
-		if(grid == null || grid.length == 0)
+		if (grid == null || grid.length == 0) {
 			return 0;
-		
-		int cnt = 0;
-		
+		}
+		int countOfFreshOranges = 0;
 		Queue<int[]> qu = new LinkedList<>();
-		
-		for(int i = 0; i < grid.length; i++) {
-			for(int j = 0; j < grid[i].length; j ++) {
-				if(grid[i][j] == 2) {
-					qu.offer(new int[] {i , j});
-				} else if(grid[i][j] == 1) {
-					cnt++;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (grid[i][j] == 2) {
+					qu.add(new int[] { i, j });
+				} else if (grid[i][j] == 1) {
+					countOfFreshOranges++;
 				}
 			}
 		}
-		
-		if(cnt == 0)
+
+		if (countOfFreshOranges == 0) {
 			return 0;
-		int count = 0;
-		
-		int[][] dirs = new int[][] {{0,1}, {1,0 },{0,-1},{-1, 0}};
-		
-		while(!qu.isEmpty()) {
-			++count;
-			int size = qu.size();
-			for(int i = 0; i < size; i++) {
+		}
+		int timeElapsed = 0;
+		int[][] dirs = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+		while (!qu.isEmpty()) {
+			timeElapsed++;
+			for (int i = 0; i < qu.size(); i++) {
 				int[] point = qu.poll();
-				for(int[] dir : dirs) {
-					int x = dir[0] + point[0];
-					int y = dir[1] + point[1];
-					if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 0 || grid[x][y] == 2) {
-						continue;
+				for (int[] dir : dirs) {
+					int x = point[0] + dir[0];
+					int y = point[1] + dir[1];
+					if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 1) {
+						grid[x][y] = 2;
+						qu.add(new int[] { x, y });
+						countOfFreshOranges--;
 					}
-					grid[x][y] = 2;
-					qu.offer(new int[] {x, y});
-					cnt--;
-					
 				}
 			}
 		}
-		return cnt == 0 ? count - 1 : -1;
-    }
+		return countOfFreshOranges == 0 ? timeElapsed - 1 : -1;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new RottingOranges().orangesRotting(new int[][] { { 2, 1, 1 }, { 1, 1, 0 }, { 0, 1, 1 } }));
+	}
 
 }
